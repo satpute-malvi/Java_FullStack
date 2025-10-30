12b5227feda379d18d9d0131ddb40acf323dd038
satpute-malvi <satputemalvi@gmail.com>
2025-10-30 17:40:41 +0530

    Add files via upload

---
+++ DatatypeMinMax.java
@@ -0,0 +1,16 @@
+
+public class DatatypeMinMax {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Range for Short Datatype =  " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+		System.out.println("Range for Byte Datatype=  " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+		System.out.println("Range for Int Datatype =  " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+		System.out.println("Range for Long Datatype =  " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+		System.out.println("Range for Float Datatype =  " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+		System.out.println("Range for Double Datatype =  " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+		System.out.println("Range for Char Datatype =  " + (int) Character.MIN_VALUE + " to " + (int) Character.MAX_VALUE);
+		System.out.println("For Boolean Range is not Fixed...!");
+	}
+
+}
