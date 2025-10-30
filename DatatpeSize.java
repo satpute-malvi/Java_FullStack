76d801da6fdee4272eaa8b66705323bbc4b510de
satpute-malvi <satputemalvi@gmail.com>
2025-10-30 17:45:32 +0530

    Add DatatpeSize class to display data type sizes

---
+++ DatatpeSize.java
@@ -0,0 +1,27 @@
+
+public class DatatpeSize {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Byte:" + Byte.BYTES);
+		System.out.println("Short:" + Short.BYTES);
+		System.out.println("Integer:" + Integer.BYTES);
+		System.out.println("Long:" + Long.BYTES);
+		System.out.println("Float" + Float.BYTES);
+		System.out.println("Double:" + Double.BYTES);
+		System.out.println("Char:" + Character.BYTES);
+		System.out.println("Not applicable for boolean");
+		
+		System.out.println("-------------------------------------------");
+		
+		System.out.println("Byte:" + Byte.SIZE);
+		System.out.println("Short:" + Short.SIZE);
+		System.out.println("Integer:" + Integer.SIZE);
+		System.out.println("Long:" + Long.SIZE);
+		System.out.println("Float" + Float.SIZE);
+		System.out.println("Double:" + Double.SIZE);
+		System.out.println("Char:" + Character.SIZE);
+		System.out.println("Not applicable for boolean");
+	}
+
+}
