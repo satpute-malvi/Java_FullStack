f31da3c0752666551c247ebcc265e95f86a51e29
satpute-malvi <satputemalvi@gmail.com>
2025-10-31 17:09:04 +0530

    Add Employee, Student, and Account Holder classes
    30th Oct assignment

---
+++ VariablePartice.java
@@ -0,0 +1,95 @@
+
+public class EmployeeInfo {
+
+	public static void main(String[] args) {
+		int employeeId = 123;              
+	    String name = "Malvi";                 
+	    String designation = "Software Engineer";          
+	    String department = "IT";           
+	    double salary = 65000.50;               
+	    String email = "malvi.satpute@company.com"; 
+	    String phoneNumber = "9876543210"; 
+	    String address = "Pune, Maharashtra";     
+	    String dateOfJoining = "2023-08-01"; 
+	    Boolean employmentStatus = true; 
+	    
+	    
+	    System.out.println("----- Employee Information -----");
+	    System.out.println("Employee id        :" + employeeId);
+	    System.out.println("Name               :" + name );
+	    System.out.println("Designation        :" + designation);
+	    System.out.println("Deppartment        :"+ department);
+	    System.out.println("Salary             :" + salary);
+	    System.out.println("Email              :" + email);
+	    System.out.println("Phone Number       :" + phoneNumber);
+	    System.out.println("Address            :" + address);
+	    System.out.println("Date of Joining    :" + dateOfJoining);
+	    System.out.println("Employment Status  :" + employmentStatus);
+	}
+
+}
+
+
+
+public class StudentInfo {
+
+	public static void main(String[] args) {
+		
+		    int studentId = 202501;
+		    String name = "Aarav Sharma";
+		    int age = 20;
+		    String gender = "Male";
+		    String course = "B.Tech Computer Science";
+		    int year = 3;
+		    String email = "aarav.sharma@college.edu";
+		    String phoneNumber = "9876541230";
+		    String address = "Nagpur, Maharashtra";
+		    double cgpa = 8.7;
+		    
+		    System.out.println("----- Student Information -----");
+	      System.out.println("Student ID      : " + studentId);
+	      System.out.println("Name            : " + name);
+	      System.out.println("Age             : " + age);
+	      System.out.println("Gender          : " + gender);
+	      System.out.println("Course          : " + course);
+	      System.out.println("Year            : " + year);
+	      System.out.println("Email           : " + email);
+	      System.out.println("Phone Number    : " + phoneNumber);
+	      System.out.println("Address         : " + address);
+	      System.out.println("CGPA            : " + cgpa);
+
+	}
+
+}
+
+
+public class AccountHolderInfo {
+	
+	public static void main(String[] args) {
+		long accountNumber = 987654321012L;
+	    String accountHolderName = "Priya Deshmukh";
+	    String accountType = "Savings";
+	    String bankName = "State Bank of India";
+	    String branchName = "Pune Main Branch";
+	    String ifscCode = "SBIN0000456";
+	    double balance = 45890.75;
+	    String phoneNumber = "9876547890";
+	    String email = "priya.deshmukh@gmail.com";
+	    boolean isActive = true;
+
+	    
+	    System.out.println("----- Account Holder Information -----");
+	    System.out.println("Account Number      : " + accountNumber);
+	    System.out.println("Account Holder Name : " + accountHolderName);
+	    System.out.println("Account Type        : " + accountType);
+	    System.out.println("Bank Name           : " + bankName);
+	    System.out.println("Branch Name         : " + branchName);
+	    System.out.println("IFSC Code           : " + ifscCode);
+	    System.out.println("Balance             : " + balance);
+	    System.out.println("Phone Number        : " + phoneNumber);
+	    System.out.println("Email               : " + email);
+	    System.out.println("Account Active      : " + isActive);
+	}
+
+}
+
